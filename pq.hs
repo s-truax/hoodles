23e7f87eed2a78eafdcc202d4264d582281947ee
Sam Truax <samuel.truax@berkeley.edu>
2020-12-04 20:12:10 -0800

    added notes

--- pq.hs
+++ pq.hs
@@ -1,3 +1,10 @@
+-- Original idea: just implement tokenizing, well-formedness, and decision
+-- procedure for the PQ formal system. Evolves into discovering that the
+-- natural first attempt of validate-then-parse can be simplified by just
+-- parsing to determine well-formedness. aka, assume that your tokens are
+-- well-formed and then try to parse them, raising an error if they are not
+-- well formed. The error case means they are not well-formed.
+
 import Data.List (group)
 
 data PQSymbol = P | Q | H deriving (Eq, Show)
@@ -14,8 +21,8 @@ tokenizePQ xs = case xs of []       -> Right []
 
 -- Assert that a PQ-string is well formed.
 -- Future Feature: tell WHY this string is not valid.
-isValidPQ :: PQString -> Bool
-isValidPQ xs = case group xs of
+isValidPQ' :: PQString -> Bool
+isValidPQ' xs = case group xs of
   [h1, [P], h2, [Q], h3] -> onlyHyphens (h1 ++ h2 ++ h3)
   otherwise              -> False
 
@@ -23,8 +30,23 @@ isValidPQ xs = case group xs of
 onlyHyphens :: PQString -> Bool
 onlyHyphens = all (== H)
 
--- Parse a list of PQ
-parsePQ :: PQString -> (Int, Int, Int)
+
+-- Count the hyphens in a PQ string. Return nothing if the entire string
+-- is not hyphens.
+countHyphens :: PQString -> Maybe Int
+countHyphens xs = case xs of []        -> Just 0
+                             (H:ys)    -> (+) <$> 1 <*> countHyphens y
+                             otherwise -> Nothing
+
+-- Parse a PQ string.
+-- Future feature: custom monad for parsing PQ strings.
+parsePQ :: PQString -> Maybe (Int, Int, Int)
+parsePQ xs = do
+  [h1, [P], h2, [Q], h3] <- return $ group xs
+  a                      <- countHyphens h1
+  b                      <- countHyphens h2
+  c                      <- countHyphens h3
+  return (a, b, c)
 
 -- TODO: Assert that a PQString is a theorem.
 -- Interestingly, in this system, it's easier to just check if something
