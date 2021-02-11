755f1a9da07d15df16914e78bc82844861417aad
Sam Truax <samuel.truax@berkeley.edu>
2021-02-10 20:33:35 -0800

    Found a bug in transfer instruction when writing sub1

--- URM.hs
+++ URM.hs
@@ -14,7 +14,7 @@ execS :: [Int] -> Int -> [Int]
 execS urm = changeRegister urm (\x -> x + 1)
 
 execT :: [Int] -> Int -> Int -> [Int]
-execT urm m = changeRegister urm (\x -> urm !! m)
+execT urm n = changeRegister urm (\x -> urm !! n)
 
 execJ urm m n q curr =
   if (urm !! m) == (urm !! n)
@@ -32,14 +32,17 @@ runProgram' urm p currInstrNum =
                             (S n)     ->
                               runProgram' (execS urm n) p (currInstrNum + 1)
                             (T n m)   ->
-                              runProgram' (execT urm m n) p (currInstrNum + 1)
+                              runProgram' (execT urm n m) p (currInstrNum + 1)
                             (J n m q) ->
                               runProgram' urm p (execJ urm n m q currInstrNum)
 
 
 -- Interactive tests
 twothree = newURM [2, 3]
+five = newURM [5]
 
 addP = [(J 2 1 4), (S 0), (S 2), (J 0 0 0)] -- Computes x + y
+sub1 = [(J 0 2 6), (S 2), (J 0 2 6), (S 1), (S 2), (J 0 0 2), (T 1 0)]
 
 test1 = runProgram twothree addP -- should output 5
+test2 = runProgram five sub1     -- expect 4
