22e6d59087db6987425c604234f3cbc84eb2d44d
Sam Truax <samuel.truax@berkeley.edu>
2021-02-19 12:59:26 -0800

    first pass at encoding URM instructions

--- FunctionCoding.hs
+++ FunctionCoding.hs
@@ -1,10 +1,16 @@
+data Instruction =
+    Z Integer
+  | S Integer
+  | T Integer Integer
+  | J Integer Integer Integer deriving (Show)
+
 -- Bijection N^2 -> N
-tau :: Int -> Int -> Int
 tau m n = 2^m * (2 * n + 1) - 1
 
 -- Inverse of tau
 -- Haskell note: if I make the type signature of this function
 -- `Int -> (Int, Int)`, I get a divide by zero error.
+-- TODO: Optimize this.
 aut r = let
   m  = length [ k | k <- [1..(r + 1)], ((r + 1) `mod` 2^k) == 0 ]
   n  = (((r + 1) `div` 2^m) - 1) `div` 2 in
@@ -18,3 +24,9 @@ atez r = let
   (tauMN, q) = aut r
   (m, n)     = aut tauMN in
   (m, n, q)
+
+beta :: Instruction -> Integer
+beta (Z n)     = 4 * (n - 1)
+beta (S n)     = 4 * (n - 1) + 1
+beta (T m n)   = 4 * tau (m - 1) (n - 1) + 2
+beta (J m n q) = 4 * zeta (m - 1) (n - 1) (q - 1) + 3
