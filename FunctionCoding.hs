3017fdc00a25350f4542cfe2457c2af379b7cd74
Sam Truax <samuel.truax@berkeley.edu>
2021-03-03 14:25:28 -0800

    wrote the function that turns numbers into URM programs

--- FunctionCoding.hs
+++ FunctionCoding.hs
@@ -11,8 +11,9 @@ tau m n = 2^m * (2 * n + 1) - 1
 -- Haskell note: if I make the type signature of this function
 -- `Int -> (Int, Int)`, I get a divide by zero error.
 -- TODO: Optimize this.
+aut :: Integer -> (Integer, Integer)
 aut r = let
-  m  = length [ k | k <- [1..(r + 1)], ((r + 1) `mod` 2^k) == 0 ]
+  m  = fromIntegral $ length [ k | k <- [1..(r + 1)], ((r + 1) `mod` 2^k) == 0 ]
   n  = (((r + 1) `div` 2^m) - 1) `div` 2 in
   (m, n)
 
@@ -20,6 +21,7 @@ aut r = let
 zeta m n q = tau (tau n m) q
 
 -- Inverse of zeta
+atez :: Integer -> (Integer, Integer, Integer)
 atez r = let
   (tauMN, q) = aut r
   (m, n)     = aut tauMN in
@@ -30,3 +32,20 @@ beta (Z n)     = 4 * (n - 1)
 beta (S n)     = 4 * (n - 1) + 1
 beta (T m n)   = 4 * tau (m - 1) (n - 1) + 2
 beta (J m n q) = 4 * zeta (m - 1) (n - 1) (q - 1) + 3
+
+ateb :: Integer -> Instruction
+ateb n = case n `mod` 4 of 0 -> Z $ n `div` 4 + 1
+                           1 -> S $ (n - 1) `div` 4 + 1
+                           2 -> uncurry T $ aut $ (n - 2) `div` 4 + 1
+                           3 -> (\(x, y, z) -> (J x y z)) $
+                                 atez $ (n - 3) `div` 4 + 1
+
+{-
+So now I should be able to write the function
+
+        | 1 if phi_n(n) converges
+f(n) =  |
+        | 0 if phi_n(n) diverges or n >= 10
+
+just by visual inspecetion of the first 10 functions.
+-}
